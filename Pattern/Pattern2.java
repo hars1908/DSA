acc4d8308f34a976b2737b67abce986858c90919
honey <t.harsh@appgallop.com>
2024-03-02 23:49:52 +0530

    Pattern2

---
+++ Pattern/Pattern2.java
@@ -0,0 +1,18 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter The Size Of row of Pattern :");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(0);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
